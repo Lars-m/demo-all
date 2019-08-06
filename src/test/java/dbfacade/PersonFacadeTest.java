b2cea1c1542a0ad28581a3b3a2bccc978baf3455
lars-m <lam@cphbusiness.dk>
2019-08-06 17:11:18 +0200

    minor

--- src/test/java/dbfacade/PersonFacadeTest.java
+++ src/test/java/dbfacade/PersonFacadeTest.java
@@ -43,7 +43,7 @@ public class PersonFacadeTest {
                 "jdbc:mysql://localhost:3306/mydb_test",
                 "dev",
                 "ax2",
-                EntityManagerFactoryCreator.Strategy.DROP_AND_CREATE);
+                EntityManagerFactoryCreator.Strategy.CREATE);
         facade = PersonFacade.getPersonFacade(emf);
     }
 
@@ -51,11 +51,13 @@ public class PersonFacadeTest {
     public void setUp() {
         EntityManager em = emf.createEntityManager();
         try {
+            System.out.println("Creating test data");
             em.getTransaction().begin();
             em.createQuery("DELETE from Person").executeUpdate();
             em.persist(new Person("a", "b", "c", "d", "a@b.dk"));
             em.persist(new Person("a2", "b2", "c2", "d2", "a2@b.dk"));
             em.getTransaction().commit();
+            System.out.println("Created test data");
         } finally {
             em.close();
         }
@@ -65,9 +67,11 @@ public class PersonFacadeTest {
     public void tearDown() {
         EntityManager em = emf.createEntityManager();
         try {
+            System.out.println("Deleting test data");
             em.getTransaction().begin();
             em.createQuery("DELETE from Person").executeUpdate();
             em.getTransaction().commit();
+            System.out.println("Deleted test data");
         } finally {
             em.close();
         }
@@ -84,6 +88,7 @@ public class PersonFacadeTest {
     @Test
     public void testGetPersons() {
         List<Person> persons = facade.getPersons();
+        System.out.println("Persons: "+persons.size());
         assertEquals(2, persons.size());
         assertEquals("a", persons.get(0).getFirstName());
         assertEquals("a2", persons.get(1).getFirstName());
